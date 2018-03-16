5842b55623e52bfccfe8e57dbee8be62e518bebe
arman <arman@DESKTOP-OQTGIRF>
2018-03-16 18:33:18 +0100

    ejercicio 2 guia 1

--- Guia5Instructoria/src/com/frames/guia1/frmEjercicio1.java
+++ Guia5Instructoria/src/com/frames/guia1/frmEjercicio1.java
@@ -9,12 +9,12 @@ package com.frames.guia1;
  *
  * @author arman
  */
-public class FrameGuia1 extends javax.swing.JFrame {
+public class frmEjercicio1 extends javax.swing.JFrame {
 
     /**
      * Creates new form FrameGuia1
      */
-    public FrameGuia1() {
+    public frmEjercicio1() {
         initComponents();
         this.setLocationRelativeTo(null);
     }
@@ -105,20 +105,21 @@ public class FrameGuia1 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrameGuia1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmEjercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrameGuia1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmEjercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrameGuia1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmEjercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrameGuia1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmEjercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrameGuia1().setVisible(true);
+                new frmEjercicio1().setVisible(true);
             }
         });
     }
