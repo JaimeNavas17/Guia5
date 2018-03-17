ab4354902ea91c4cbb71dbebebeb9094870ef560
armando <asus>
2018-03-17 17:46:04 -0600

    desarrolladores

--- Guia5Instructoria/src/com/frames/login/frmLogin.java
+++ Guia5Instructoria/src/com/frames/login/frmLogin.java
@@ -38,6 +38,9 @@ public class frmLogin extends javax.swing.JFrame {
         txtLogin = new javax.swing.JTextField();
         btnIngresar = new javax.swing.JButton();
         txtPass = new javax.swing.JPasswordField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(300, 250));
@@ -61,6 +64,15 @@ public class frmLogin extends javax.swing.JFrame {
         });
         getContentPane().add(btnIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 170, -1, -1));
         getContentPane().add(txtPass, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 120, 130, -1));
+
+        jLabel4.setText("Esperanza Herrera Armando Jose");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 210, -1, 30));
+
+        jLabel5.setText("Espadero Miranda Edgardo Ernesto");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 230, -1, 30));
+
+        jLabel6.setText("Desarolladores:");
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 190, -1, 30));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -125,6 +137,9 @@ public class frmLogin extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JTextField txtLogin;
     private javax.swing.JPasswordField txtPass;
     // End of variables declaration//GEN-END:variables
